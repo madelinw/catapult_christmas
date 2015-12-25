dac9af4bbdb6c5a119aebd4ea4483ef721a7ed9c
Madelin Woods <madelinw@gmail.com>
2015-12-25 12:09:39 -0500

    mwahaha present delivery takeover

--- index.js
+++ index.js
@@ -20,29 +20,29 @@ var pinata = Bodies.rectangle(450, 300, 80, 100, {
 });
 pinata.hitsLeft = 3;
 
-var mickey = Bodies.circle(400, 300, 50, {
-    render: { sprite: { texture: './mickey.png' } },
+var sushi = Bodies.circle(400, 300, 50, {
+    render: { sprite: { texture: './sushi.png' } },
     collisionFilter: {
         category: draggableCategory,
-    },                           
+    },
 });
-var airplane= Bodies.circle(350, 300, 50, {
-    render: { sprite: { texture: './airplane.png' } },
+var riceball= Bodies.circle(350, 300, 50, {
+    render: { sprite: { texture: './riceball.png' } },
     collisionFilter: {
         category: draggableCategory,
-    },                           
+    },
 });
 var santa = Bodies.circle(450, 300, 50, {
-    render: { sprite: { texture: './santa.png' } },
+    render: { sprite: { texture: './hearteyes.png' } },
     collisionFilter: {
         category: draggableCategory,
-    },                           
+    },
 });
 
 
 var roof = Bodies.rectangle(400, 0, 810, 60, {isStatic: true});
 var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
-var magnifier = Bodies.rectangle(400, 180, 80, 80, { 
+var magnifier = Bodies.rectangle(400, 180, 80, 80, {
     isStatic: true ,
     render: { sprite: { texture: './magnifying.png' } }
 });
@@ -50,14 +50,14 @@ var left = Bodies.rectangle(0, 0, 60, 1600, {isStatic: true});
 var right = Bodies.rectangle(810 , 0, 60, 1600, {isStatic: true});
 
 var anchor = {x: 400, y: 30};
-var string =  Constraint.create({ 
-    pointA: anchor, 
-    bodyB: pinata, 
-    stiffness: 0.9, 
-    render: { 
-        lineWidth: 5, 
-        strokeStyle: '#dfa417' 
-    } 
+var string =  Constraint.create({
+    pointA: anchor,
+    bodyB: pinata,
+    stiffness: 0.9,
+    render: {
+        lineWidth: 5,
+        strokeStyle: '#dfa417'
+    }
 });
 var mouseConstraint = MouseConstraint.create(engine, {
     collisionFilter: {
@@ -65,23 +65,23 @@ var mouseConstraint = MouseConstraint.create(engine, {
     }
 });
 
-var rockOptions = { 
-    density: 0.004, 
+var rockOptions = {
+    density: 0.004,
     collisionFilter: {
         category: draggableCategory,
-    },                           
-    render: { /*sprite: { texture: './img/rock.png' } (*/ } 
+    },
+    render: { /*sprite: { texture: './img/rock.png' } (*/ }
 };
 var     rock = Bodies.polygon(170, 450, 8, 20, rockOptions),
         anchor = { x: 170, y: 450 },
-        elastic = Constraint.create({ 
-            pointA: anchor, 
-            bodyB: rock, 
-            stiffness: 0.05, 
-            render: { 
-                lineWidth: 5, 
-                strokeStyle: '#000000' 
-            } 
+        elastic = Constraint.create({
+            pointA: anchor,
+            bodyB: rock,
+            stiffness: 0.05,
+            render: {
+                lineWidth: 5,
+                strokeStyle: '#000000'
+            }
         });
 
 // add all of the bodies to the world
@@ -92,22 +92,22 @@ World.add(engine.world, [
 
 var isFinderGame = false;
 function finderGame() {
-    mickey.velocity = {x: 2 * Math.random(), y: 2 * Math.random()};
-    airplane.velocity = {x: 2 * Math.random(), y: 2 * Math.random()};
+    sushi.velocity = {x: 2 * Math.random(), y: 2 * Math.random()};
+    riceball.velocity = {x: 2 * Math.random(), y: 2 * Math.random()};
     santa.velocity = {x: 2 * Math.random(), y: 2 * Math.random()};
     World.remove(engine.world, [pinata, string, rock, elastic]);
-    World.add(engine.world, [mickey, airplane, santa]);
+    World.add(engine.world, [sushi, riceball, santa]);
     var i = 0;
     for (i = 0; i < 50; i++) {
         var circle = Bodies.circle(400, 300, Math.random() * 20 + 5);
         circle.velocity = {x: 2 * Math.random(), y: 2 * Math.random()};
         World.add(engine.world, circle);
     }
-    setTimeout(function() { 
+    setTimeout(function() {
         showMessage("Drag to magnify!");
-        World.add(engine.world, magnifier); 
+        World.add(engine.world, magnifier);
     }, 1000);
-    isFinderGame = true; 
+    isFinderGame = true;
 }
 
 function showMessage(message) {
@@ -133,13 +133,13 @@ Events.on(engine, 'collisionStart', function(event) {
                 }
             }
         } else if (
-            pair.bodyA == mickey && pair.bodyB == magnifier ||
-            pair.bodyA == magnifier && pair.bodyB == mickey) {
-            showMessage("Hong Kong Disneyland ticket");
+            pair.bodyA == sushi && pair.bodyB == magnifier ||
+            pair.bodyA == magnifier && pair.bodyB == sushi) {
+            showMessage("Sushi & Dumpling Class");
         } else if (
-            pair.bodyA == airplane && pair.bodyB == magnifier ||
-            pair.bodyA == magnifier && pair.bodyB == airplane) {
-            showMessage("UA179 to Hong Kong (01/29-02/07)");
+            pair.bodyA == riceball && pair.bodyB == magnifier ||
+            pair.bodyA == magnifier && pair.bodyB == riceball) {
+            showMessage("Sunday, January 3rd, 8pm");
         } else if (
             pair.bodyA == santa && pair.bodyB == magnifier ||
             pair.bodyA == magnifier && pair.bodyB == santa) {
